81f38d5ee977c12fc17c8f930c914f6015d15f25
Sahil <sahilsangwan1926@gmail.com>
2021-04-07 22:14:52 +0530

    init commit

---
+++ firebase_init.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD4JyUN0YYhElMFfGBSXzTmQvjkX8qUEjI",
+    authDomain: "website-design-6adf2.firebaseapp.com",
+    projectId: "website-design-6adf2",
+    storageBucket: "website-design-6adf2.appspot.com",
+    messagingSenderId: "938344268679",
+    appId: "1:938344268679:web:f2755542052864bdfa8922",
+    measurementId: "G-Z4BLS72J9P"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
